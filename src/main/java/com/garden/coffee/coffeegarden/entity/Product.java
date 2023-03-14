3de743bc9b7c77119e9ac12a94db7cf245864789
garden <gkdlqkql6@naver.com>
2023-03-14 20:14:19 +0900

    엔티티 생성

---
+++ src/main/java/com/garden/coffee/coffeegarden/entity/Product.java
@@ -0,0 +1,11 @@
+package com.garden.coffee.coffeegarden.entity;
+
+public class Product {
+    public String name;
+    public Long price;
+
+    public Product(String name, Long price) {
+        this.name = name;
+        this.price = price;
+    }
+}
