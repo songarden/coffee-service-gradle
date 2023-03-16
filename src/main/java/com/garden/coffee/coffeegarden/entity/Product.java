582d21c137a5f1358f0cca46ee416e76fa4c88db
garden <gkdlqkql6@naver.com>
2023-03-16 16:59:03 +0900

    카테고리,할인 필드 추가/ 생성자에도 추가 반영

--- src/main/java/com/garden/coffee/coffeegarden/entity/Product.java
+++ src/main/java/com/garden/coffee/coffeegarden/entity/Product.java
@@ -1,11 +1,17 @@
 package com.garden.coffee.coffeegarden.entity;
 
+
+
 public class Product {
     public String name;
     public Long price;
+    public Category category;
+    public Long sale;
 
-    public Product(String name, Long price) {
+    public Product(String name, Long price, Category category, Long sale) {
         this.name = name;
         this.price = price;
+        this.category = category;
+        this.sale = sale;
     }
 }
