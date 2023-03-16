dde722e5af4bc93b23a77a1a547062e88ff2bc95
garden <gkdlqkql6@naver.com>
2023-03-16 17:02:10 +0900

    save 인자를 필드 값 -> 객체 전체로 변경

--- src/main/java/com/garden/coffee/coffeegarden/service/ProductService.java
+++ src/main/java/com/garden/coffee/coffeegarden/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.garden.coffee.coffeegarden.service;
 
 
+import com.garden.coffee.coffeegarden.entity.Category;
 import com.garden.coffee.coffeegarden.entity.Product;
 import com.garden.coffee.coffeegarden.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,8 @@ public class ProductService {
     ProductRepository productRepository;
 
 
-    public Product save(String name, Long price){
-        return productRepository.save(name,price);
+    public Product save(Product product){
+        return productRepository.save(product);
     }
 
     public ArrayList<Product> search(Long productId){
