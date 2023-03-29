25d4a017629dbf2bc9ba2455ff3981274069cfb2
garden <gkdlqkql6@naver.com>
2023-03-29 13:56:51 +0900

    GetAll / GetById / Post / Delete 로직 구현 -> 결과값으로 HttpStatusCode and Enum값 + 로직 처리완료된 데이터 제이슨 화 제공

--- src/main/java/com/garden/coffee/coffeegarden/product/ProductController.java
+++ src/main/java/com/garden/coffee/coffeegarden/product/ProductController.java
@@ -1,27 +1,56 @@
 package com.garden.coffee.coffeegarden.product;
 
 
+import com.garden.coffee.coffeegarden.DtoList;
+import com.garden.coffee.coffeegarden.response.Response;
+import com.garden.coffee.coffeegarden.response.ResponseDto;
+import com.garden.coffee.coffeegarden.response.ResponseStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
     @Autowired
     ProductService productService;
 
-    @PostMapping("/products")
-    public Product apiSave(@RequestParam("name") String name, @RequestParam("price") Long price, @RequestParam("category")Category category,@RequestParam("sale")Long sale){
-        Product product = new Product(name, price, category, sale);
-        return productService.save(product);
-    }
-
     @GetMapping("/products")
-    public Product apiSearch(@RequestParam("product-id") Long productId){
-        return productService.search(productId);
+    public Response<DtoList<ProductDto>> apiSearchAll(){
+        DtoList<ProductDto> searchingProductList = productService.getAllProduct();
+        ResponseStatus responseStatus = ResponseStatus.CREATE_DONE;
+        if(searchingProductList == null){
+            responseStatus = ResponseStatus.CREATE_FAIL;
+        }
+        return new ResponseDto<DtoList<ProductDto>>(responseStatus,searchingProductList).toResponse();
     }
 
-    @DeleteMapping("/products")
-    public Product apiDelete(@RequestParam("product-id") Long productId){
-        return productService.delete(productId);
+    @GetMapping("/products/product-id")
+    public Response<ProductDto> apiSearch(@RequestParam("v") String productId){
+        ProductDto searchingProduct = productService.getProduct(productId);
+        ResponseStatus responseStatus = ResponseStatus.CREATE_DONE;
+        if(searchingProduct == null){
+            responseStatus = ResponseStatus.CREATE_FAIL;
+        }
+        return new ResponseDto<ProductDto>(responseStatus,searchingProduct).toResponse();
+    }
+    @PostMapping("/products")
+    public Response<ProductDto> apiSave(@RequestBody ProductDto productDto){
+        ProductDto savingProduct = productService.save(productDto);
+        ResponseStatus responseStatus = ResponseStatus.CREATE_DONE;
+        if(savingProduct == null){
+            responseStatus = ResponseStatus.CREATE_FAIL;
+        }
+        return new ResponseDto<ProductDto>(responseStatus,savingProduct).toResponse();
+    }
+
+    @DeleteMapping("/products/product-id")
+    public Response<ProductDto> apiDelete(@RequestParam("v") String productId){
+        ProductDto deletingProduct = productService.deleteProduct(productId);
+        ResponseStatus responseStatus = ResponseStatus.CREATE_DONE;
+        if(deletingProduct == null){
+            responseStatus = ResponseStatus.CREATE_FAIL;
+        }
+        return new ResponseDto<ProductDto>(responseStatus,deletingProduct).toResponse();
     }
 }
