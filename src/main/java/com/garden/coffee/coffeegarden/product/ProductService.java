94ac789b97b2dc5842f143e0ca5796af5ec04f80
garden <gkdlqkql6@naver.com>
2023-03-29 13:58:59 +0900

    GetAll / GetById / Post / Delete 로직 구현 -> 로직 처리 완료된 데이터 제공 -> 만약 null일 경우 controller에서 status control

--- src/main/java/com/garden/coffee/coffeegarden/product/ProductService.java
+++ src/main/java/com/garden/coffee/coffeegarden/product/ProductService.java
@@ -1,25 +1,49 @@
 package com.garden.coffee.coffeegarden.product;
 
 
-import com.garden.coffee.coffeegarden.product.Product;
-import com.garden.coffee.coffeegarden.product.ProductRepository;
+import com.garden.coffee.coffeegarden.DtoList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ProductService {
     @Autowired
-    ProductRepository productRepository;
+    ProductJPARepository productRepository;
 
-    public Product save(Product product){
-        return productRepository.save(product);
+    public DtoList<ProductDto> getAllProduct() {
+        DtoList<ProductDto> gettingProductList = new DtoList<>(productRepository.findAll());
+        return gettingProductList;
+    }
+    public ProductDto getProduct(String productId){
+        Optional<Product> optProduct = productRepository.findById(productId);
+        if(optProduct.isEmpty()){
+            return null;
+        }
+        ProductDto gettingProduct = new ProductDto(optProduct.get());
+        return gettingProduct;
+
+    }
+    public ProductDto save(ProductDto productDto){
+        productDto.setProductId(UUID.randomUUID().toString().replace("-",""));
+        productRepository.save(productDto.toEntity());
+        return productDto;
     }
 
-    public Product search(Long productId){
-        return productRepository.search(productId);
+
+
+    public ProductDto deleteProduct(String productId){
+        Optional<Product> optProduct = productRepository.findById(productId);
+        if(optProduct.isEmpty()){
+            return null;
+        }
+        ProductDto deletingProduct = new ProductDto(optProduct.get());
+        productRepository.deleteById(productId);
+        return deletingProduct;
     }
 
-    public Product delete(Long productId){
-        return productRepository.delete(productId);
-    }
+
 }
