cca9519d0c9bfe610a6885bcbf4f39869a15c066
garden <gkdlqkql6@naver.com>
2023-03-29 13:55:35 +0900

    Entity 파일 -> 빌더만 명시, 테이블 생성 어노테이션 명시 / Setter는 Dto에서 기능 수행

--- src/main/java/com/garden/coffee/coffeegarden/product/Product.java
+++ src/main/java/com/garden/coffee/coffeegarden/product/Product.java
@@ -1,16 +1,38 @@
 package com.garden.coffee.coffeegarden.product;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="product_table")
+@Getter
+@NoArgsConstructor
+@Data
 public class Product {
-    public String name;
-    public Long price;
-    public Category category;
-    public Long sale;
+    @Id
+    @Column(name="product_id")
+    private String productId;
+    @Column(name="product_name")
+    private String productName;
 
-    public Product(String name, Long price, Category category, Long sale) {
-        this.name = name;
-        this.price = price;
-        this.category = category;
-        this.sale = sale;
+    @Column(name="product_price")
+    private Long productPrice;
+    @Column(name="sale_percent")
+    private Long salePercent;
+
+    @Builder
+    public Product(String productId,String productName, Long productPrice, Long salePercent){
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.salePercent = salePercent;
     }
+
 }
