520d614c78bceb74b78e727bef4443a2fa71fe5f
garden <gkdlqkql6@naver.com>
2023-04-18 21:57:34 +0900

    Order 도메인에서 상품명을 받아오기 위해 메소드 생성

--- src/main/java/com/garden/coffee/coffeegarden/product/ProductJPARepository.java
+++ src/main/java/com/garden/coffee/coffeegarden/product/ProductJPARepository.java
@@ -1,11 +1,11 @@
 package com.garden.coffee.coffeegarden.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 
 @Repository
 public interface ProductJPARepository extends JpaRepository<Product,String> {
-
+    Optional<Product> findByProductName(String productName);
 }
