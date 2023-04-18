0f56a436c0f7c593a1d939117828741f9a4efabf
garden <gkdlqkql6@naver.com>
2023-04-18 21:53:22 +0900

    Order 도메인 Entity 생성

--- src/main/java/com/garden/coffee/coffeegarden/order/Order.java
+++ src/main/java/com/garden/coffee/coffeegarden/order/Order.java
@@ -1,16 +1,51 @@
 package com.garden.coffee.coffeegarden.order;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 
 
-//@Entity
+@Entity
+@Table(name="orders")
 @Getter
 @NoArgsConstructor
 @Data
 public class Order {
+
+    @Id
+    @Column(name="order_id")
+    private String orderId;
+
+    @Column(name="order_price")
+    private int orderPrice;
+
+    @Column(name="product_name")
+    private String productName;
+
+    @Column(name="address")
+    private String address;
+
+    @Column(name="order_time")
+    private LocalDateTime orderTime;
+
+    @Column(name="order_state")
+    private OrderState orderState;
+
+    @Builder
+    public Order(String orderId, int orderPrice, String productName, String address, LocalDateTime orderTime, OrderState orderState) {
+        this.orderId = orderId;
+        this.orderPrice = orderPrice;
+        this.productName = productName;
+        this.address = address;
+        this.orderTime = orderTime;
+        this.orderState = orderState;
+    }
 }
