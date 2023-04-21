ff6e026f2e8c1949fc221f6a0779d974948d62ab
garden <gkdlqkql6@naver.com>
2023-04-21 16:20:36 +0900

    Post 요청 시 장바구니처럼 여러개의 상품별로 할인율 계산하여 총 금액 저장하는 로직 생성

--- src/main/java/com/garden/coffee/coffeegarden/order/OrderService.java
+++ src/main/java/com/garden/coffee/coffeegarden/order/OrderService.java
@@ -2,14 +2,12 @@ package com.garden.coffee.coffeegarden.order;
 
 import com.garden.coffee.coffeegarden.DtoList;
 import com.garden.coffee.coffeegarden.product.Product;
-import com.garden.coffee.coffeegarden.product.ProductDto;
-import com.garden.coffee.coffeegarden.product.ProductJPARepository;
+import com.garden.coffee.coffeegarden.product.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class OrderService {
@@ -17,7 +15,7 @@ public class OrderService {
     @Autowired
     OrderRepository orderRepository;
     @Autowired
-    ProductJPARepository productRepository;
+    ProductRepository productRepository;
 
     public DtoList<OrderDto> getAllOrders() {
         DtoList<OrderDto> allOrders = new DtoList<>(orderRepository.findAll());
@@ -35,17 +33,30 @@ public class OrderService {
 
     public OrderDto createOrder(OrderDto orderDto) {
         OrderDto creatingOrder = orderDto;
-        Optional<Product> optOrderedProduct = productRepository.findByProductName(creatingOrder.productName);
-        if(optOrderedProduct.isEmpty()){
-            return null;
+        List<Product> products = new ArrayList<>();
+        for(Product product : creatingOrder.getProducts()){
+            Optional<Product> optProduct = productRepository.findByProductName(product.getProductName());
+            if(optProduct.isEmpty()){
+                return null;
+            }
+            products.add(optProduct.get());
         }
-        ProductDto orderedProduct = new ProductDto(optOrderedProduct.get());
-        int discountedPrice = orderedProduct.getProductPrice();
-        int salePercent = orderedProduct.getSalePercent();
-        if (salePercent > 0) {
-            discountedPrice -= (discountedPrice * salePercent) / 100;
-        }
-        creatingOrder.setOrderPrice(discountedPrice);
+        creatingOrder.setProducts(products);
+
+        creatingOrder.orderPrice = 0;
+        int orderPriceSum = creatingOrder.orderPrice; //주문 상품들의 할인된 금액 합계 저장할 변수
+
+        for(Product product : products){
+            int productPrice = product.getProductPrice();
+            int salePercent = product.getSalePercent();
+            if(salePercent > 0){
+                productPrice -= (productPrice * salePercent) / 100;
+            }
+            orderPriceSum += productPrice;
+        } //Product에서 금액과 할인율을 받아 실제 금액을 계산한 후, 각각을 합계 변수에 금액 축적
+        //한개의 주문에 여러개의 상품이 들어 있을 수도 있음 ( 장바구니 서비스 )
+
+        creatingOrder.setOrderPrice(orderPriceSum);
         creatingOrder.setOrderId(UUID.randomUUID().toString().replace("-",""));
         creatingOrder.setOrderTime(LocalDateTime.now());
         creatingOrder.setOrderState(OrderState.PLACED);
